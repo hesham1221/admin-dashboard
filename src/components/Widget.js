c70d634cdb196939c6ae1b311979f074f0ed5772
hesham1221 <hishammohmed12345@gmail.com>
2022-04-27 04:42:39 +0200

    finishing widget

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,22 +1,47 @@
-import { KeyboardArrowUp, PersonOutlined } from '@mui/icons-material'
-import React from 'react'
-import './widget.scss'
-const Widget = () => {
+import {
+  AccountBalance,
+  AddShoppingCartOutlined,
+  IndeterminateCheckBoxOutlined,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  Paid,
+  PersonOutlined,
+} from "@mui/icons-material";
+import React from "react";
+import "./widget.scss";
+const Widget = ({ type, isMoney, num, status, lastTotal }) => {
+  const Icon = () => {
+    switch (type) {
+      case "Users":
+        return <PersonOutlined className="icon" />;
+      case "Orders":
+        return <AddShoppingCartOutlined className="icon" />;
+      case "Earnings":
+        return <Paid className="icon" />;
+      case "Balance":
+        return <AccountBalance className="icon"/>;
+        default :
+        return <IndeterminateCheckBoxOutlined />
+    }
+  };
   return (
-    <div className='widget'>
-        <div className="left">
-            <span className="title">USERS</span>
-            <span className="counter">21354</span>
-            <span className="link">See all users</span>
+    <div className="widget">
+      <div className="left">
+        <span className="title">{type}</span>
+        <span className="counter">{`${num}${isMoney ? "$" : ""}`}</span>
+        <span className="link">See all {type}</span>
+      </div>
+      <div className="right">
+        <div className={`percentage ${status}`}>
+          {status === "positive" ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+          {status === "positive"
+            ? `${Math.round(((num - lastTotal) / lastTotal) * 100)}%`
+            : `${Math.round(((lastTotal - num) / lastTotal) * 100)}%`}
         </div>
-        <div className="right">
-            <div className="percentage">
-                <KeyboardArrowUp />
-            20%</div>
-            <PersonOutlined className='icon'/>
-        </div>
+        <Icon/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Widget
+export default Widget;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,50 @@
-import React from 'react'
-import NavBar from '../../components/NavBar'
-import Sidebar from '../../components/Sidebar'
-import Widget from '../../components/Widget'
-import './home.scss'
+import React from "react";
+import NavBar from "../../components/NavBar";
+import Sidebar from "../../components/Sidebar";
+import Widget from "../../components/Widget";
+import "./home.scss";
 const Home = () => {
+  const widgedFetched = [
+    {
+      type: "Users",
+      isMoney: false,
+      num: 100,
+      status : 'positive',
+      lastTotal : 80
+    },
+    {
+      type: "Orders",
+      isMoney: false,
+      num: 80,
+      status : 'negative',
+      lastTotal : 120
+    },
+    {
+      type: "Earnings",
+      isMoney: true,
+      num: 20,
+      status : 'negative',
+      lastTotal : 60
+    },
+    {
+      type: "Balance",
+      isMoney: true,
+      num: 130,
+      status : 'positive',
+      lastTotal : 120
+    },
+  ];
   return (
-    <div className='home'>
-        <Sidebar />
-        <div className="home-container">
-          <NavBar/>
-            <div className="widgets">
-              <Widget />
-              <Widget />
-              <Widget />
-              <Widget />
-            </div>
+    <div className="home">
+      <Sidebar />
+      <div className="home-container">
+        <NavBar />
+        <div className="widgets">
+          {widgedFetched.map(widged => <Widget {...widged} />)}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
